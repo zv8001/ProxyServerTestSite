c4a6fe138079305012e898eb97c60056c7b94c00
𝓥𝓮𝔁𝓣𝓱𝓮𝓟𝓻𝓸𝓽𝓸𝓰𝓮𝓷 <149710902+zv8001@users.noreply.github.com>
2026-03-18 22:11:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const BackendBaseUrl = "Will add link back once we secure the proxy servers";
+const BackendBaseUrl = "https://utstatuswebsitelink0128.ututcproxyservers2819.com/";
 const PollIntervalMs = 5000;
 
 function SetCardState(CardId, BadgeId, StatusText, StateClass) {
